2aab62cda8714c0497400466418f1d37542a3783
fpsetd <xiaoxionghui@live.com>
2018-03-26 02:03:23 +0800

    补充提交

--- src/main/resources/sql/test-data.sql
+++ src/main/resources/sql/test-data.sql
@@ -1,9 +1,9 @@
 insert into `user`
 	(`account`, `nickname`, `password`, `avatar`)
 values
-	('xiaoqi', '小奇', 'xiaoqi123', '/static/images/account1.jpg'),
-	('maolaoshi', '猫老师', 'maolaoshi123', '/static/images/account2.jpg'),
-	('kedaya', '可达鸭', 'kedaya123', '/static/images/account3.jpg'),
-	('ali', '阿狸', 'ali123', '/static/images/account4.jpg'),
-	('pikaqiu', '皮卡丘', 'pikaqiu123', '/static/images/account5.jpg'),
-	('longmao', '龙猫', 'longmao123', '/static/images/account6.jpg');
+	('xiaoqi', '小奇', 'xiaoqi123', '/static/images/xiaoqi.jpg'),
+	('maolaoshi', '猫老师', 'maolaoshi123', '/static/images/maolaoshi.jpg'),
+	('kedaya', '可达鸭', 'kedaya123', '/static/images/kedaya.jpg'),
+	('ali', '阿狸', 'ali123', '/static/images/ali.jpg'),
+	('pikaqiu', '皮卡丘', 'pikaqiu123', '/static/images/pikaqiu.jpg'),
+	('longmao', '龙猫', 'longmao123', '/static/images/longmao.jpg');
